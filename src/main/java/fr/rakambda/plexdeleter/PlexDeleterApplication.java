c4bdfc9df0403f1ab047c619481b96c21c028097
Thomas Couchoud <1688389+Rakambda@users.noreply.github.com>
2024-03-10 20:32:24 +0100

    Add aot hints for thymeleaf

--- src/main/java/fr/rakambda/plexdeleter/PlexDeleterApplication.java
+++ src/main/java/fr/rakambda/plexdeleter/PlexDeleterApplication.java
@@ -2,6 +2,7 @@ package fr.rakambda.plexdeleter;
 
 import fr.rakambda.plexdeleter.aot.JacksonHints;
 import fr.rakambda.plexdeleter.aot.ResourceHints;
+import fr.rakambda.plexdeleter.aot.ThymeleafHints;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -15,7 +16,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication(scanBasePackages = "fr.rakambda.plexdeleter")
 @ImportRuntimeHints({
 		JacksonHints.class,
-		ResourceHints.class
+		ResourceHints.class,
+		ThymeleafHints.class
 })
 public class PlexDeleterApplication{
 	public static void main(String[] args){
---
+++ src/main/java/fr/rakambda/plexdeleter/aot/ThymeleafHints.java
@@ -0,0 +1,26 @@
+package fr.rakambda.plexdeleter.aot;
+
+import fr.rakambda.plexdeleter.service.UserService;
+import fr.rakambda.plexdeleter.storage.entity.MediaEntity;
+import fr.rakambda.plexdeleter.storage.entity.UserPersonEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.springframework.aot.hint.ExecutableMode;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+
+@Slf4j
+public class ThymeleafHints implements RuntimeHintsRegistrar{
+	@Override
+	public void registerHints(@NotNull RuntimeHints hints, @Nullable ClassLoader classLoader){
+		try{
+			hints.reflection().registerMethod(UserService.class.getMethod("getSoonDeletedMedias"), ExecutableMode.INVOKE);
+			hints.reflection().registerMethod(UserService.class.getMethod("getMediaOverseerrUrl", MediaEntity.class), ExecutableMode.INVOKE);
+			hints.reflection().registerMethod(UserService.class.getMethod("getUserMedias", UserPersonEntity.class), ExecutableMode.INVOKE);
+		}
+		catch(NoSuchMethodException e){
+			throw new RuntimeException(e);
+		}
+	}
+}
