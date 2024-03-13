c96e9e7a71e248709090d8d4254f0c0e2785d30d
Thomas Couchoud <1688389+Rakambda@users.noreply.github.com>
2024-03-13 19:45:53 +0100

    Clean messages and get lang from header

--- src/main/java/fr/rakambda/plexdeleter/web/WebConfiguration.java
+++ src/main/java/fr/rakambda/plexdeleter/web/WebConfiguration.java
@@ -5,17 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import java.util.List;
 import java.util.Locale;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer{
 	@Bean
 	public LocaleResolver localeResolver(){
-		var slr = new SessionLocaleResolver();
-		slr.setDefaultLocale(Locale.US);
-		return slr;
+		var acceptHeaderLocaleResolver = new AcceptHeaderLocaleResolver();
+		acceptHeaderLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+		acceptHeaderLocaleResolver.setSupportedLocales(List.of(Locale.ENGLISH, Locale.FRENCH));
+		return acceptHeaderLocaleResolver;
 	}
 	
 	@Bean
