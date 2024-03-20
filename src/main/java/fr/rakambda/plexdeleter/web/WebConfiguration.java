56e33ed39fb7b7280e339a6ac4cfcc4ff8e508a6
Thomas Couchoud <108809129+couchoud-t@users.noreply.github.com>
2024-03-20 12:58:43 +0100

    Log raw requests

--- src/main/java/fr/rakambda/plexdeleter/web/WebConfiguration.java
+++ src/main/java/fr/rakambda/plexdeleter/web/WebConfiguration.java
@@ -2,6 +2,7 @@ package fr.rakambda.plexdeleter.web;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -30,5 +31,16 @@ public class WebConfiguration implements WebMvcConfigurer{
 	@Override
 	public void addInterceptors(InterceptorRegistry registry){
 		registry.addInterceptor(localeChangeInterceptor());
+	}
+	
+	@Bean
+	public CommonsRequestLoggingFilter logFilter() {
+		var filter = new CommonsRequestLoggingFilter();
+		filter.setIncludeQueryString(true);
+		filter.setIncludePayload(true);
+		filter.setMaxPayloadLength(10000);
+		filter.setIncludeHeaders(false);
+		filter.setAfterMessagePrefix("REQUEST DATA: ");
+		return filter;
 	}
 }
