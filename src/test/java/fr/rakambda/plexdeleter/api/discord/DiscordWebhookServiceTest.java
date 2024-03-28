742dcee7f9fd29508bb39289dfdc550d7c7c1965
Thomas Couchoud <108809129+couchoud-t@users.noreply.github.com>
2024-03-28 10:30:24 +0100

    Add discord webhook ratelimit test

--- src/test/java/fr/rakambda/plexdeleter/api/discord/DiscordWebhookServiceTest.java
+++ src/test/java/fr/rakambda/plexdeleter/api/discord/DiscordWebhookServiceTest.java
@@ -8,6 +8,9 @@ import fr.rakambda.plexdeleter.api.discord.data.WebhookMessage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
@@ -58,5 +61,25 @@ class DiscordWebhookServiceTest{
 		
 		assertThatCode(() -> tested.sendWebhookMessage(SecretsUtils.getSecret("discord.webhook-forum"), result.getChannelId(), messageReply))
 				.doesNotThrowAnyException();
+	}
+	
+	@Test
+	void itShouldHandleRateLimit(){
+		try(var pool = Executors.newVirtualThreadPerTaskExecutor()){
+			
+			var tasks = IntStream.rangeClosed(1, 20)
+					.mapToObj(index -> WebhookMessage.builder().content("this is a rate limit test %d".formatted(index)).build())
+					.map(message -> (Callable<Boolean>) () -> {
+						tested.sendWebhookMessage(SecretsUtils.getSecret("discord.webhook"), message);
+						return true;
+					})
+					.map(pool::submit)
+					.toList();
+			
+			assertThat(tasks).allSatisfy(future -> {
+				assertThatCode(future::get).doesNotThrowAnyException();
+				assertThat(future.resultNow()).isTrue();
+			});
+		}
 	}
 }
