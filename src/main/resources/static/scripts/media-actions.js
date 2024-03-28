ae1d86560afdd674f34900dd35977270a4004140
Thomas Couchoud <1688389+Rakambda@users.noreply.github.com>
2024-03-28 13:10:32 +0100

    Fix buttons deactivation

--- src/main/resources/static/scripts/media-actions.js
+++ src/main/resources/static/scripts/media-actions.js
@@ -1,21 +1,7 @@
-(() => {
-    'use strict'
-
-    function onButtonClick(btn) {
-        document.querySelectorAll('.media-action-button')
-                .forEach(btn2 => {
-                    btn2.setAttribute('disabled', 'disabled')
-                })
-    }
-
-    function registerButton(btn) {
-        btn.addEventListener('click', () => onButtonClick(btn))
-    }
-
-    function documentLoaded() {
-        document.querySelectorAll('.media-action-button')
-                .forEach(btn => registerButton(btn))
-    }
-
-    window.addEventListener('DOMContentLoaded', () => documentLoaded())
-})()
+function onMediaActionFormSubmit() {
+    document.querySelectorAll('.media-action-button')
+            .forEach(btn2 => {
+                btn2.setAttribute('disabled', 'disabled')
+            })
+    return true;
+}
