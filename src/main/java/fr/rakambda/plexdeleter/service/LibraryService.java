29f9e19e89607c65fcca4107220064b5b2b1553a
Thomas Couchoud <1688389+Rakambda@users.noreply.github.com>
2024-07-08 19:18:42 +0200

    Sort library unmapped

--- src/main/java/fr/rakambda/plexdeleter/service/LibraryService.java
+++ src/main/java/fr/rakambda/plexdeleter/service/LibraryService.java
@@ -12,63 +12,66 @@ import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Stream;
 
 @Slf4j
 @Service
-public class LibraryService {
-    private final ApplicationConfiguration applicationConfiguration;
-    private final TautulliService tautulliService;
-    private final MediaRepository mediaRepository;
-
-    @Autowired
-    public LibraryService(ApplicationConfiguration applicationConfiguration, TautulliService tautulliService, MediaRepository mediaRepository) {
-        this.applicationConfiguration = applicationConfiguration;
-        this.tautulliService = tautulliService;
-        this.mediaRepository = mediaRepository;
-    }
-
-    @NotNull
-    public Collection<LibraryElement> getAllLibraryContentWithoutMedia() {
-        var libraryElements = Optional.ofNullable(applicationConfiguration.getPlex().getTemporaryLibraries())
-                .stream()
-                .flatMap(Collection::stream)
-                .flatMap(this::getLibraryContent)
-                .toList();
-
-        return libraryElements.stream()
-                .filter(this::isRecordMissing)
-                .toList();
-    }
-
-    private boolean isRecordMissing(@NotNull LibraryElement element) {
-        return !mediaRepository.existsByRootPlexId(element.ratingKey());
-    }
-
-    @NotNull
-    private Stream<LibraryElement> getLibraryContent(int section) {
-        try {
-            return Optional.ofNullable(tautulliService.getLibraryMediaInfo(section).getResponse())
-                    .map(TautulliResponse::getData)
-                    .map(GetLibraryMediaInfo::getData).stream()
-                    .flatMap(Collection::stream)
-                    .map(record -> new LibraryElement(
-                            record.getRatingKey(),
-                            record.getTitle(),
-                            switch (record.getMediaType()) {
-                                case SHOW, EPISODE, SEASON -> MediaType.SEASON;
-                                case MOVIE -> MediaType.MOVIE;
-                                case PHOTO, TRACK, ARTIST -> null;
-                            }
-                    ))
-                    .filter(element -> Objects.nonNull(element.type()));
-        } catch (RequestFailedException e) {
-            log.error("Failed to get library content for section id {}", section, e);
-            return Stream.empty();
-        }
-    }
+public class LibraryService{
+	private final ApplicationConfiguration applicationConfiguration;
+	private final TautulliService tautulliService;
+	private final MediaRepository mediaRepository;
+	
+	@Autowired
+	public LibraryService(ApplicationConfiguration applicationConfiguration, TautulliService tautulliService, MediaRepository mediaRepository){
+		this.applicationConfiguration = applicationConfiguration;
+		this.tautulliService = tautulliService;
+		this.mediaRepository = mediaRepository;
+	}
+	
+	@NotNull
+	public Collection<LibraryElement> getAllLibraryContentWithoutMedia(){
+		var libraryElements = Optional.ofNullable(applicationConfiguration.getPlex().getTemporaryLibraries())
+				.stream()
+				.flatMap(Collection::stream)
+				.flatMap(this::getLibraryContent)
+				.toList();
+		
+		return libraryElements.stream()
+				.filter(this::isRecordMissing)
+				.sorted(Comparator.comparing(LibraryElement::type)
+						.thenComparing(LibraryElement::name))
+				.toList();
+	}
+	
+	private boolean isRecordMissing(@NotNull LibraryElement element){
+		return !mediaRepository.existsByRootPlexId(element.ratingKey());
+	}
+	
+	@NotNull
+	private Stream<LibraryElement> getLibraryContent(int section){
+		try{
+			return Optional.ofNullable(tautulliService.getLibraryMediaInfo(section).getResponse())
+					.map(TautulliResponse::getData)
+					.map(GetLibraryMediaInfo::getData).stream()
+					.flatMap(Collection::stream)
+					.map(record -> new LibraryElement(
+							record.getRatingKey(),
+							record.getTitle(),
+							switch(record.getMediaType()){
+								case SHOW, EPISODE, SEASON -> MediaType.SEASON;
+								case MOVIE -> MediaType.MOVIE;
+								case PHOTO, TRACK, ARTIST -> null;
+							}
+					))
+					.filter(element -> Objects.nonNull(element.type()));
+		}
+		catch(RequestFailedException e){
+			log.error("Failed to get library content for section id {}", section, e);
+			return Stream.empty();
+		}
+	}
 }
