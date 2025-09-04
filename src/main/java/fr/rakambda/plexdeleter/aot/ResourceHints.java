8ac34f8fd285a8d89423b46d4d825b1656bcb64a
Thomas Couchoud <1688389+Rakambda@users.noreply.github.com>
2025-09-04 18:58:49 +0200

    Register gql resources

--- src/main/java/fr/rakambda/plexdeleter/aot/ResourceHints.java
+++ src/main/java/fr/rakambda/plexdeleter/aot/ResourceHints.java
@@ -6,6 +6,8 @@ import org.springframework.aot.hint.RuntimeHintsRegistrar;
 public class ResourceHints implements RuntimeHintsRegistrar{
 	@Override
 	public void registerHints(RuntimeHints hints, ClassLoader classLoader){
+		hints.resources().registerPattern("api/plex/community/gql/*");
+		hints.resources().registerPattern("api/plex/community/gql/fragment/*");
 		hints.resources().registerPattern("lang/*");
 	}
 }
