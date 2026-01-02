c08f89935eee2f77d89a11a77079920808aabfbe
Thomas Couchoud <1688389+Rakambda@users.noreply.github.com>
2026-01-02 08:59:57 +0100

    Try to improve UTF-8 support

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,8 +77,9 @@ tasks {
 tasks.withType<BootBuildImage> {
     builder = "paketobuildpacks/builder-jammy-full:latest"
     environment = mapOf(
-        "BP_NATIVE_IMAGE_BUILD_ARGUMENTS" to "-H:+AddAllCharsets -march=compatibility",
-        "BPE_LANG" to "C.UTF-8",
+        "BP_NATIVE_IMAGE_BUILD_ARGUMENTS" to "-H:+AddAllCharsets -march=compatibility -Dfile.encoding=UTF-8",
+        "BPE_LANG" to "en_US.UTF-8",
+        "BPE_LC_ALL" to "en_US.UTF-8",
         "BPE_APPEND_JAVA_TOOL_OPTIONS" to "-Duser.language=en -Duser.country=US -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8",
         "BPE_DELIM_JAVA_TOOL_OPTIONS" to " "
     )
