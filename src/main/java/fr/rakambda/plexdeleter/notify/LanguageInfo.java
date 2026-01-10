0601444c030eb53dcd53811362adf2d65b96253f
Thomas Couchoud <1688389+Rakambda@users.noreply.github.com>
2026-01-10 14:03:54 +0100

    RegisterReflectionForBinding for LanguageInfo

--- src/main/java/fr/rakambda/plexdeleter/notify/LanguageInfo.java
+++ src/main/java/fr/rakambda/plexdeleter/notify/LanguageInfo.java
@@ -2,7 +2,9 @@ package fr.rakambda.plexdeleter.notify;
 
 import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 
+@RegisterReflectionForBinding(LanguageInfo.class)
 public record LanguageInfo(
 		@NonNull String translationKey,
 		@Nullable String flagUrl
