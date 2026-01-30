c9ab329553b5b231cfbbb736b1081395d34ff901
Thomas Couchoud <1688389+Rakambda@users.noreply.github.com>
2026-01-30 09:37:06 +0100

    Avoid sending empty subtitles languages

--- src/main/java/fr/rakambda/plexdeleter/api/tautulli/data/SubtitlesMediaPartStream.java
+++ src/main/java/fr/rakambda/plexdeleter/api/tautulli/data/SubtitlesMediaPartStream.java
@@ -1,12 +1,14 @@
 package fr.rakambda.plexdeleter.api.tautulli.data;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import fr.rakambda.plexdeleter.json.EmptyStringAsNullDeserializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.jspecify.annotations.Nullable;
 import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
+import tools.jackson.databind.annotation.JsonDeserialize;
 
 @Data
 @NoArgsConstructor
@@ -19,9 +21,11 @@ public class SubtitlesMediaPartStream extends MediaPartStream{
 	private String subtitleCodec;
 	@JsonProperty("subtitle_container")
 	@Nullable
+	@JsonDeserialize(using = EmptyStringAsNullDeserializer.class)
 	private String subtitleContainer;
 	@JsonProperty("subtitle_format")
 	@Nullable
+	@JsonDeserialize(using = EmptyStringAsNullDeserializer.class)
 	private String subtitleFormat;
 	@JsonProperty("subtitle_forced")
 	private int subtitleForced;
@@ -30,8 +34,10 @@ public class SubtitlesMediaPartStream extends MediaPartStream{
 	private String subtitleLocation;
 	@JsonProperty("subtitle_language")
 	@Nullable
+	@JsonDeserialize(using = EmptyStringAsNullDeserializer.class)
 	private String subtitleLanguage;
 	@JsonProperty("subtitle_language_code")
 	@Nullable
+	@JsonDeserialize(using = EmptyStringAsNullDeserializer.class)
 	private String subtitleLanguageCode;
 }
